9d54bfa6cc8a3e9679542ecf7fb8b6cf17afc8ba
feifeiq <feifeiq@foxmail.com>
2015-06-18 14:22:25 +0800

    #

--- src/trait_obj.rs
+++ src/trait_obj.rs
@@ -9,6 +9,7 @@ impl Foo for u8 {
 impl Foo for String {
 	fn method(&self) -> String { format!("string: {}", *self) }
 }
+
 // fn do_something<T: Foo>(x: T) {
 // 	x.method();
 // }
@@ -19,11 +20,12 @@ fn do_something(x: &Foo) {
 
 
 fn main() {
-	/// let x = 5;
-	/// #let y = 6;
-	/// println!("{}", x + y);
 	let x = 5u8;
 	do_something(&x as &Foo);
+	println!("{:?}", x);
+
+	let x = "Hello".to_string();
+	do_something(&x);
 	println!("{:?}", x);
 
 	let y={
